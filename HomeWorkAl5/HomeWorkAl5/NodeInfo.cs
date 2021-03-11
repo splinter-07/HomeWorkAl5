8eca0417d4f29112473e45f36b69b3594b13965d
splinter-07 <splinter_07@list.ru>
2021-03-11 18:26:23 +0300

    HomeWorkAl5

---
+++ HomeWorkAl5/HomeWorkAl5/NodeInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWorkAl5
+{
+    public class NodeInfo
+    {
+        public int Depth { get; set; }
+        public TreeNode Node { get; set; }
+    }
+}
---
+++ HomeWorkAl5/HomeWorkAl5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWorkAl5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            tree.AddItem(10);
+            tree.AddItem(100);
+            tree.AddItem(1);
+            tree.AddItem(4);
+            tree.AddItem(50);
+            tree.AddItem(0);
+            tree.AddItem(19);
+            tree.AddItem(45);
+            tree.AddItem(17);
+
+            tree.PrintTree();
+            Console.WriteLine();
+
+            Console.WriteLine($"BFS: \nОжидание:  [10]  [1]  [100]  [0]  [4]  [50]  [19]  [17]  [45]");
+            Console.Write("Реальность: ");
+            tree.BreadthFirstSearch();
+            Console.WriteLine();
+
+            Console.WriteLine($"DFS: \nОжидание:  [10]  [1]  [0]  [4]  [100]  [50]  [19]  [17]  [45]");
+            Console.Write("Реальность: ");
+            tree.DeepFirstSearch();
+            
+        }
+
+    }
+
+    
+
+    
+    
+
+    
+}
